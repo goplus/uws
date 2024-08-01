e6616a081b6d4e685561042cd37e9e45469bf1d5
xushiwei <x@goplus.org>
2024-08-01 13:53:19 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 cd build.dir
-# TODO
+SRC=../src
+UWSDIR=../uWebSockets
+FLAGS="-march=native -O3 -Wpedantic -Wall -Wextra -Wsign-conversion -Wconversion -std=c++20 -I$UWSDIR/src -I$UWSDIR/uSockets/src -flto uSockets/*.o -lz"
+
+echo "==> g++ $FLAGS $SRC/HelloWorld.cpp -o HelloWorld"
+g++ $FLAGS $SRC/HelloWorld.cpp -o HelloWorld
+
+echo "==> g++ $FLAGS $SRC/Client.cpp -o Client"
+g++ $FLAGS $SRC/Client.cpp -o Client
